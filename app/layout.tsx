93886d40cc5d9263b5c5cb84a09d1313856bbaec
yishak <yishakwondimgezahu12@gmail.com>
2025-01-30 21:07:48 +0300

    I have done the authentication part with firebase

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
+"use client";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { AuthContextProvider } from "@/context/AuthContext";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,7 +29,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <AuthContextProvider>{children}</AuthContextProvider>
       </body>
     </html>
   );
